e3413c69e437c9be7eac1c58f63587f800a678de
Pratikshiku <15362748867@163.com>
2021-01-07 17:43:47 +0800

    开辟微服务 order - 30007 management - 30008

---
+++ provider-customer-order-30007/src/main/java/top/ptcc9/ProviderCustomerOrder30007.java
@@ -0,0 +1,16 @@
+package top.ptcc9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: HE LONG CAN
+ * @description:
+ * @date: 2021-01-07 17:30
+ */
+@SpringBootApplication
+public class ProviderCustomerOrder30007 {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderCustomerOrder30007.class,args);
+    }
+}
---
+++ provider-management-30008/src/main/java/top/ptcc9/ProviderManagement30008.java
@@ -0,0 +1,17 @@
+package top.ptcc9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: HE LONG CAN
+ * @description:
+ * @date: 2021-01-07 17:38
+ */
+
+@SpringBootApplication
+public class ProviderManagement30008 {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderManagement30008.class,args);
+    }
+}
